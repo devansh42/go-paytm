1608f1854401abc19847710fc4017c6fa0ae0e2b
aang <devanshguptamrt@gmail.com>
2020-01-21 19:18:53 +0530

    config updated

--- config.go
+++ config.go
@@ -1,11 +1,37 @@
 package paytm
 
-const (
-	PaytmMerchantKey = `xxxxxxxxx`
-	MID              = `xxxxxxxxxxxxxxxxxxxx`
-	INDUSTRY_TYPE_ID = `Retail`
-	CHANNEL_ID       = `WAP`
-	WEBSITE          = `APPSTAGING`
-	CALLBACK_URL     = `https://securegw-stage.paytm.in/theia/paytmCallback?ORDER_ID=`
+var (
+	paytmMerchantKey     = `xxxxxxxxx`
+	mID                  = `xxxxxxxxxxxxxxxxxxxx`
+	iNDUSTRY_TYPE_ID     = `Retail`
+	cHANNEL_ID           = `WAP`
+	wEBSITE              = `APPSTAGING`
+	CALLBACK_URL         = `https://securegw-stage.paytm.in/theia/paytmCallback?ORDER_ID=`
 	TransactionStatusAPI = `https://securegw-stage.paytm.in/merchant-status/getTxnStatus`
 )
+
+//SetPaytmMerchantKey, sets paytm's merchant key
+func SetPaytmMerchantKey(key string) {
+	paytmMerchantKey = key
+}
+
+//SetMID, sets paytm's Merchant ID
+func SetMID(mid string) {
+	mID = mid
+}
+
+//SetIndustryTypeId, sets Industry Type Id
+func SetIndustryTypeId(id string) {
+	iNDUSTRY_TYPE_ID = id
+}
+
+//SetChannerlId, sets channel Id
+func SetChannerlId(cid string) {
+	cHANNEL_ID = cid
+}
+
+//SetWebsite, sets host website
+//Use APPSTAGING for development purposes
+func SetWebsite(web string) {
+	wEBSITE = web
+}
